9ddbb597a316e9d2e64968d008a3643e411aacd5
usmanqadeer <usmanqadeer.4006@gmail.com>
2020-12-18 01:03:02 -0800

    Create App.xaml.cs

---
+++ KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using KidsStoriesApp.Views;
+using Xamarin.Forms;
+
+namespace KidsStoriesApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainMenuPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
