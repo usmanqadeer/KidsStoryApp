0777c5ada4f559c478bcc64fed6076c7c7ef5a37
usmanqadeer <usmanqadeer.4006@gmail.com>
2020-12-18 01:02:31 -0800

    Create PlayListPage.xaml.cs

---
+++ KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/Views/PlayListPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace KidsStoriesApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PlayListPage : ContentPage
+    {
+        ItemList ItemList = new ItemList();
+        public PlayListPage()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+        public List<ItemList> PropertyTypeList => ItemList.AllStories();
+    }
+}
