7b07100782841ebe5b418dc60d7f3d3a7ae9ee81
usmanqadeer <usmanqadeer.4006@gmail.com>
2020-12-18 12:31:01 -0800

    Update App.xaml.cs

--- KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/App.xaml.cs
+++ KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/App.xaml.cs
@@ -1,10 +1,25 @@
-﻿using KidsStoriesApp.Views;
+﻿using KidsStoriesApp.Data;
+using KidsStoriesApp.Views;
 using Xamarin.Forms;
 
 namespace KidsStoriesApp
 {
     public partial class App : Application
     {
+        private static KidsStoriesDataBase storiesDataBase;
+
+        public static  KidsStoriesDataBase KidsStoriesDataBase
+        {
+            get
+            {
+                if (storiesDataBase == null)
+                {
+                    storiesDataBase = new KidsStoriesDataBase();
+                }
+                return storiesDataBase;
+            }
+
+        }
         public App()
         {
             InitializeComponent();
