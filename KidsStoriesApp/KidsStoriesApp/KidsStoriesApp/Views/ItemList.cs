03c4c4d1e28fb7523fef95bcb047105697e4e47c
usmanqadeer <usmanqadeer.4006@gmail.com>
2020-12-18 01:02:45 -0800

    Create ItemList.cs

---
+++ KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/Views/ItemList.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace KidsStoriesApp.Views
+{
+    public class ItemList
+    {
+        List<ItemList> itemLists = new List<ItemList>();
+        public int PlayID { get; set; }
+        public string StoryTitel { get; set; }
+       
+        public List<ItemList> AllStories()
+        {
+            List<ItemList> itemList = new List<ItemList>()
+        {
+            new ItemList{PlayID = 1, StoryTitel ="As you design your document and make formatting decisions, you will need to consider line and paragraph spacing."},
+            new ItemList{PlayID = 2, StoryTitel ="As you design your document and make formatting decisions, you will need to consider line and paragraph spacing."},
+            new ItemList{PlayID = 3, StoryTitel ="As you design your document and make formatting decisions, you will need to consider line and paragraph spacing."}
+        };
+            List<ItemList> lists = itemList;
+            foreach(ItemList item in lists)
+            {
+                itemLists.Add(item);
+            }
+            return itemLists;
+        }
+    }
+}
