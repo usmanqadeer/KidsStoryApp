24ce7a278b8c814eb0e34c12f3f9152ac201998d
usmanqadeer <usmanqadeer.4006@gmail.com>
2020-12-18 12:30:25 -0800

    Create StoriesData.cs

---
+++ KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/Services/StoriesData.cs
@@ -0,0 +1,36 @@
+﻿using KidsStoriesApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KidsStoriesApp.Services
+{
+    public class StoriesData : IKidsStoriesListDataStore<KidsStoriesList>
+    {
+        readonly List<KidsStoriesList> storiesLists;
+        public Task<bool> AddStoriesAsync(KidsStoriesList story)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteStoriesAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> GetStoriesAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<KidsStoriesList>> GetStoriesAsync(bool forceRefresh = false)
+        {
+            return await App.KidsStoriesDataBase.GetkidsStoriesAsync();
+        }
+
+        public Task<bool> UpdateStoriesAsync(KidsStoriesList story)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
