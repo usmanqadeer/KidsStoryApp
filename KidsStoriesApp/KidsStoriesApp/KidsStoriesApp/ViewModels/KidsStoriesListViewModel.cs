3148440429a3ef02e4b17a54f5038587872644ee
usmanqadeer <usmanqadeer.4006@gmail.com>
2020-12-19 11:51:40 -0800

    Create KidsStoriesListViewModel.cs

---
+++ KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/ViewModels/KidsStoriesListViewModel.cs
@@ -0,0 +1,33 @@
+﻿using KidsStoriesApp.Models;
+using KidsStoriesApp.Services;
+using System;
+using System.Collections.ObjectModel;
+
+namespace KidsStoriesApp.ViewModels
+{
+    public class KidsStoriesListViewModel : BaseViewModel
+    {
+        public ObservableCollection<KidsStoriesListModel> KidsStories { get; set; }
+        private IKidsStoriesListDataStore _kidsStoriesListDataStore;
+        public KidsStoriesListViewModel(IKidsStoriesListDataStore kidsStoriesListDataStore)
+        {
+            KidsStories = new ObservableCollection<KidsStoriesListModel>();
+            _kidsStoriesListDataStore = kidsStoriesListDataStore;
+            GetStoriesAsync();
+        }
+        public async void GetStoriesAsync()
+        {
+            try
+            {
+                var allStories = await _kidsStoriesListDataStore.GetAllStories();
+                foreach (var stories in allStories)
+                {
+                    KidsStories.Add(stories);
+                }
+            }
+            catch (Exception ex) { }
+        }
+
+
+    }
+}
