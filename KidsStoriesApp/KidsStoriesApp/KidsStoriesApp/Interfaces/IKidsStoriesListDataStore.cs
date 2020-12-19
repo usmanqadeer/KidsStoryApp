cec43eb407182171de2b072e2f992b0b3b548bfb
usmanqadeer <usmanqadeer.4006@gmail.com>
2020-12-19 11:52:24 -0800

    Create IKidsStoriesListDataStore.cs

---
+++ KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/Interfaces/IKidsStoriesListDataStore.cs
@@ -0,0 +1,11 @@
+﻿using KidsStoriesApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KidsStoriesApp.Services
+{
+    public interface IKidsStoriesListDataStore
+    {
+       Task<List<KidsStoriesListModel>> GetAllStories();
+    }
+}
