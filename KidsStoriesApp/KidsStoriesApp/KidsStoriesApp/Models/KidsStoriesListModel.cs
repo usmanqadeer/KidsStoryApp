4ad09ea1f0b103df87bbf580bfa4ad7afd031ca2
usmanqadeer <usmanqadeer.4006@gmail.com>
2020-12-19 11:52:06 -0800

    Create KidsStoriesListModel.cs

---
+++ KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/Models/KidsStoriesListModel.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace KidsStoriesApp.Models
+{
+    public class KidsStoriesListModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string StoryTitel { get; set; }
+        public string StoryText { get; set; }
+    }
+}
