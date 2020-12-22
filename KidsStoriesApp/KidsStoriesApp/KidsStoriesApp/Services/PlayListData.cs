260cd6b40bfd537d5f853d327bb98e930f704fda
usmanqadeer <usmanqadeer.4006@gmail.com>
2020-12-22 04:40:40 -0800

    Create PlayListData.cs

---
+++ KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/Services/PlayListData.cs
@@ -0,0 +1,15 @@
+﻿using KidsStoriesApp.Interfaces;
+using KidsStoriesApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KidsStoriesApp.Services
+{
+    public class PlayListData : IPlayListDataStore
+    {
+        public async Task<List<RecordStoriesListModel>> GetAllPlayList()
+        {
+            return await App.KidsStoriesDataBase.GetAllPlayListAsync();
+        }
+    }
+}
