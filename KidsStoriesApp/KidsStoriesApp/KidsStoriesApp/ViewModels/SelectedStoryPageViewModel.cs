9c55c89aa6f8a76414cfa415f272acbf041f504c
usmanqadeer <usmanqadeer.4006@gmail.com>
2020-12-22 04:40:23 -0800

    Create SelectedStoryPageViewModel.cs

---
+++ KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/ViewModels/SelectedStoryPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KidsStoriesApp.ViewModels
+{
+    public class SelectedStoryPageViewModel : BaseViewModel
+    {
+        public SelectedStoryPageViewModel()
+        {
+
+        }
+    }
+}
