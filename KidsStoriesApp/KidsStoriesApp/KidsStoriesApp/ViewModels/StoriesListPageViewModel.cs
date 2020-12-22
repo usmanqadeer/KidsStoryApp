22bbbeede23e123774cc3d876e3639542564c400
usmanqadeer <usmanqadeer.4006@gmail.com>
2020-12-22 04:40:11 -0800

    Create StoriesListPageViewModel.cs

---
+++ KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/ViewModels/StoriesListPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using KidsStoriesApp.Models;
+using KidsStoriesApp.Services;
+using System;
+using System.Collections.ObjectModel;
+
+namespace KidsStoriesApp.ViewModels
+{
+    public class StoriesListPageViewModel : BaseViewModel
+    {
+        public ObservableCollection<KidsStoriesListModel> KidsStories { get; set; }
+        private IKidsStoriesDataStore _kidsStoriesListDataStore;
+        public StoriesListPageViewModel(IKidsStoriesDataStore kidsStoriesListDataStore)
+        {
+            KidsStories = new ObservableCollection<KidsStoriesListModel>();
+            _kidsStoriesListDataStore = kidsStoriesListDataStore;
+            GetStoriesAsync();
+        }
+        public async void GetStoriesAsync()
+        {
+            try
+            {
+                var allStories = await _kidsStoriesListDataStore.GetAllStories();
+                foreach (var stories in allStories)
+                {
+                    KidsStories.Add(stories);
+                }
+            }
+            catch (Exception ex) { }
+        }
+
+
+    }
+}
