90d02236d1552b211969a7a1c86528700c63070f
usmanqadeer <usmanqadeer.4006@gmail.com>
2020-12-22 04:40:32 -0800

    Create MainPageViewModel.cs

---
+++ KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using KidsStoriesApp.Views;
+using Xamarin.Forms;
+
+namespace KidsStoriesApp.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        public Command NavigateToPlayList { get; set; }
+        public Command NavigateToStories { get; set; }
+        private INavigation _navigation;
+        public MainPageViewModel(INavigation navigation)
+        {
+            NavigateToPlayList = new Command(NaviGateToPlayListPage);
+            NavigateToStories = new Command(NavigateToStoriesListPage);
+           _navigation = navigation;
+            
+        }
+
+        private void NavigateToStoriesListPage()
+        {
+            _navigation.PushAsync(new StoriesListPage());
+        }
+
+        private void NaviGateToPlayListPage()
+        {
+            _navigation.PushAsync(new PlayListPage());
+        }
+    }
+}
