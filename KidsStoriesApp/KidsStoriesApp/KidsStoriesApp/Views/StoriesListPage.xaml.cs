2dabcb3024b3fce06903da891489c5bb7353101c
usmanqadeer <usmanqadeer.4006@gmail.com>
2020-12-22 04:39:29 -0800

    Create StoriesListPage.xaml.cs

---
+++ KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/Views/StoriesListPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using CommonServiceLocator;
+using KidsStoriesApp.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace KidsStoriesApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StoriesListPage : ContentPage
+    {
+        public StoriesListPage()
+        {
+            InitializeComponent();
+            BindingContext = ServiceLocator.Current.GetInstance(typeof(StoriesListPageViewModel));
+        }
+    }
+}
