77063a450dd91f7f7add4f4204d8a8b68412766a
usmanqadeer <usmanqadeer.4006@gmail.com>
2020-12-22 04:40:19 -0800

    Create PlayListPageViewModel.cs

---
+++ KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/ViewModels/PlayListPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using KidsStoriesApp.Interfaces;
+using KidsStoriesApp.Models;
+using System;
+using System.Collections.ObjectModel;
+
+namespace KidsStoriesApp.ViewModels
+{
+    public class PlayListPageViewModel
+    {
+        public ObservableCollection<RecordStoriesListModel> PlayListStories { get; set; }
+        private IPlayListDataStore _playListStoriesListDataStore;
+        public PlayListPageViewModel(IPlayListDataStore playListDataStore)
+        {
+            PlayListStories = new ObservableCollection<RecordStoriesListModel>();
+            _playListStoriesListDataStore = playListDataStore;
+            GetPlayListAsync();
+        }
+        public async void GetPlayListAsync()
+        {
+            try
+            {
+                var allStories = await _playListStoriesListDataStore.GetAllPlayList();
+                foreach (var stories in allStories)
+                {
+                    PlayListStories.Add(stories);
+                }
+            }
+            catch (Exception ex) { }
+        }
+    }
+}
