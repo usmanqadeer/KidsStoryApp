a3d32428b7289e64b93a3a3c2a68b279fcf822cb
usmanqadeer <usmanqadeer.4006@gmail.com>
2020-12-24 23:38:34 -0800

    Update StoriesListPage.xaml.cs

--- KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/Views/StoriesListPage.xaml.cs
+++ KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/Views/StoriesListPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using CommonServiceLocator;
+using KidsStoriesApp.Models;
 using KidsStoriesApp.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -8,10 +9,30 @@ namespace KidsStoriesApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class StoriesListPage : ContentPage
     {
+        private int id;
+        private string storyName;
+        private string storyTitel;
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string StoryText
+        {
+            get { return storyName; }
+            set { storyName = value; }
+        }
         public StoriesListPage()
         {
+
             InitializeComponent();
             BindingContext = ServiceLocator.Current.GetInstance(typeof(StoriesListPageViewModel));
         }
+       
+        private async void TapGestureRecognizer_Tapped(object sender, System.EventArgs e)
+        {
+            await Navigation.PushModalAsync(new SelectedStoryPage(storyName));
+        }
+
     }
 }
