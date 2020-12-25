f87183b4a1553f3fb04ce8392fc4d2dfa72afcdc
usmanqadeer <usmanqadeer.4006@gmail.com>
2020-12-24 23:39:23 -0800

    Create AudioFilePath.cs

---
+++ KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/Data/AudioFilePath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace KidsStoriesApp.Data
+{
+    public class AudioFilePath
+    {
+        public AudioFilePath()
+        {
+            string DataBasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "AudioFile");
+            Assembly assembly = IntrospectionExtensions.GetTypeInfo(typeof(App)).Assembly;
+            Stream ambededDataBaseStreem = assembly.GetManifestResourceStream("KidsStoriesApp.AudioFile");
+
+            if (!File.Exists(DataBasePath))
+            {
+                FileStream fileStreamToWrite = File.Create(DataBasePath);
+                ambededDataBaseStreem.Seek(0, SeekOrigin.Begin);
+                ambededDataBaseStreem.CopyTo(fileStreamToWrite);
+                fileStreamToWrite.Close();
+            }
+        }
+    }
+}
