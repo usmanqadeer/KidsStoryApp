d6dc1f1f88dccb3ae477498aab91129d6e3278f5
usmanqadeer <usmanqadeer.4006@gmail.com>
2020-12-28 11:06:18 -0800

    Update PlayListPage.xaml.cs

--- KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/Views/PlayListPage.xaml.cs
+++ KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/Views/PlayListPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using CommonServiceLocator;
-using KidsStoriesApp.Models;
+﻿using KidsStoriesApp.Models;
 using KidsStoriesApp.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,8 +11,50 @@ namespace KidsStoriesApp.Views
         public PlayListPage()
         {
             InitializeComponent();
-            BindingContext = ServiceLocator.Current.GetInstance(typeof(PlayListPageViewModel));
+            this.BindingContext = new PlayListPageViewModel(Navigation);
             
+        }
+
+        private async void btnBack_Clicked(object sender, System.EventArgs e)
+        {
+            await  this.Navigation.PopAsync();
+        }
+
+        private async void btnDeleteAudio_Clicked(object sender, System.EventArgs e)
+        {
+            try
+            {
+                var RecordStoriesListModel = (sender as View).BindingContext as RecordStoriesListModel;
+                var _storyID = await App.KidsStoriesDataBase.DeletekidsRecordStoriesAsync(RecordStoriesListModel);
+                if (_storyID > 0)
+                {
+                   await DisplayAlert("Deleted!!", "Story Delete succesfully","Cancel", "ok");
+                }
+            }
+            catch (System.Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        private async void btnPlayStory_Clicked(object sender, System.EventArgs e)
+        {
+            try
+            { 
+                var kidsRecordStoriesListModel = (sender as View).BindingContext as RecordStoriesListModel;
+                await this.Navigation.PushAsync(new PlayAudioPage(kidsRecordStoriesListModel));
+            }
+            catch (System.Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        private async void btnAddNew_Clicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushModalAsync(new StoriesListPage());
         }
     }
 }
