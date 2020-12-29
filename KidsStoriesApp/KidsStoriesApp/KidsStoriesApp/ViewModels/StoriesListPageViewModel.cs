88760474f3c14ffcf522a822246d4d16fe961d72
usmanqadeer <usmanqadeer.4006@gmail.com>
2020-12-29 03:57:27 -0800

    Update StoriesListPageViewModel.cs

--- KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/ViewModels/StoriesListPageViewModel.cs
+++ KidsStoriesApp/KidsStoriesApp/KidsStoriesApp/ViewModels/StoriesListPageViewModel.cs
@@ -8,46 +8,51 @@ namespace KidsStoriesApp.ViewModels
 {
     public class StoriesListPageViewModel : BaseViewModel
     {
-        public ObservableCollection<KidsStoriesListModel> KidsStories { get; private set; }
+        //public ObservableCollection<KidsStoriesListModel> KidsStories { get; private set; }
+        private ObservableCollection<KidsStoriesListModel> _KidsStories;
+
+        public ObservableCollection<KidsStoriesListModel> KidsStories
+        {
+            get { return _KidsStories; }
+            set { _KidsStories = value; OnPropertyChanged("KidsStories"); }
+        }
+
         public Command NavigateCommand { get; private set; }
         public Command NavigateBackCommand { get; private set; }
-        private INavigation navigation;
-
         private int _id;
 
         public int ID
         {
             get { return _id; }
-            set { _id = value; }
+            set { _id = value; OnPropertyChanged("ID"); }
         }
         private string _storyTitel;
 
         public string StoryTitel
         {
             get { return _storyTitel; }
-            set { _storyTitel = value; }
+            set { if (_storyTitel != null) _storyTitel = value; OnPropertyChanged("StoryTitel"); }
         }
         private string _storyText;
         public string StoryText
         {
             get { return _storyText; }
-            set { _storyText = value; }
+            set {if(_storyText != null) _storyText = value; OnPropertyChanged("StoryText"); }
         }
-        public StoriesListPageViewModel(INavigation _navigation)
+        public StoriesListPageViewModel()
         {
             this.KidsStories = new ObservableCollection<KidsStoriesListModel>();
-            this.navigation = _navigation;
             NavigateCommand = new Command(AddStory);
             GetStoriesAsync();
         }
-        public async void GetStoriesAsync()
+        public  async void GetStoriesAsync()
         {
             try
             {
                 var allStories = await App.KidsStoriesDataBase.GetkidsStoriesAsync();
                 foreach (var stories in allStories)
                 {
-                    KidsStories.Add(stories);
+                    _KidsStories.Add(stories);
                 }
             }
             catch (Exception ex) { }
